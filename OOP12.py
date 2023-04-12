1dcfe6816e7fb06ecd20b40055f308cb33af9dcb
Farnaz <farnazfawadhasan@gmail.com>
2023-04-12 14:15:29 +0600

    added

--- OOP12.py
+++ OOP12.py
@@ -1,33 +1,36 @@
+# Class or Static Variable
+
 class Player:
+    team_run = 0    # class / static variable
+    def __init__(self, run):   #constructor
+        self.run = run  # instance variable
+        # self.team_run = 0
 
-    team_run = 0
 
-    def __init__(self, run):
-        self.run = run
-        #self.team_run = 0
-
-    def hit_four(self):
+    def hit_four(self):     #instance method
         self.run += 4
+        # self.team_run += 4
         Player.team_run += 4
 
     def hit_six(self):
         self.run += 6
-       # self.run += 6
+        # self.team_run += 6
         Player.team_run += 6
 
-
-#=========================================
+print(Player.team_run)
 sakib = Player(0)
 tamim = Player(0)
-
+print("Sakib : ", sakib.run)
+print("Tamim : ", tamim.run)
+tamim.hit_four()
+tamim.hit_four()
 sakib.hit_six()
-sakib.hit_four()
-tamim.hit_six()
+print("Sakib : ", sakib.run)
+print("Tamim : ", tamim.run)
 
-print("Sakib:" , sakib.run)
-print("Tamim", tamim.run)
-print("Team run", Player.team_run)
-print("Sakib contribution to team", sakib.team_run)
+print("Tamim :", tamim.__dict__)
+print("Sakib :", sakib.__dict__)
 
-print("Tamim", tamim.__dict__)
-print("Sakib", sakib.__dict__)
+#print("Team Run : ", tamim.team_run)
+
+print("Team Run : ", Player.team_run)
