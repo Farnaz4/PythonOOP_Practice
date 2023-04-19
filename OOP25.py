0c64d993deb571bace42e4feb010f94396ddfc6d
Farnaz <farnazfawadhasan@gmail.com>
2023-04-20 00:11:55 +0600

    added on 20/4/23

--- OOP25.py
+++ OOP25.py
@@ -1,1 +1,31 @@
 #Abstract Class
+from abc import ABC, abstractmethod
+
+class A(ABC):
+
+    @abstractmethod
+    def method1(self):
+        pass
+#===================================
+
+class B(A):
+
+    def method1(self):
+        print("Method 1 is overridden")
+
+#==========================================
+# class B(A):
+#
+#     @abstractmethod
+#     def method2(self):
+#         pass
+# #==========================================
+# class C(B):
+#
+#     def method3(self):
+#         pass
+
+
+
+b = B()
+b.method1()
---
+++ OOP26.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+    def eat(self):
+        print("I am eating")
+#==========================================
+
+class Dog(Animal):
+    #pass
+    def make_sound(self):
+        print("WOOF WOOF")
+
+#===============================================
+class Cat(Animal):
+    def make_sound(self):
+        print("Meow Meow")
+
+#==============================================
+class Snake(Animal):
+    def make_sound(self):
+        print("Hiss Hiss")
+
+#============================================
+
+d1 = Dog()
+d1.eat()
+d1.make_sound()
+c1 = Cat()
+c1.make_sound()
+s1 = Snake()
+s1.make_sound()
